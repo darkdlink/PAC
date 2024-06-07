5534713b02edb612f5d5f30b0f332fe120631974
darkdlink <luisfelipe.fachinidark@gmail.com>
2024-06-06 21:16:37 -0300

    6/6

---
+++ lar-renascer/src/pages/benfeitores.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/button"
+import { Input } from "../components/input"
+import { useForm } from "react-hook-form"
+import { ajax, apiResponse } from "../ajax/ajax.mts";
+export function Benfeitores() {
+    return (
+        <div>
+            Benfeitores
+        </div>
+    )
+}
